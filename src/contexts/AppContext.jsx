01c905b91efe7ffb677b75571f35cac56458a82e
Lucas Ferreira <lucas6rodrigues@yahoo.com.br>
2025-09-27 02:33:05 -0300

    Estado duplicado

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,35 +1,35 @@
 import { createContext, useState, useEffect } from "react";
-import {getApiData} from "../services/ApiServices";
+import { getApiData } from "../services/ApiServices";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-    const savedLanguage = localStorage.getItem('lang')
-    const [language, setLanguage] = useState(savedLanguage ?? 'br')
-    const [languages, setLanguages] = useState()
-    const [loading, setLoading] = useState(true)
+  const savedLanguage = localStorage.getItem("lang");
+  const [language, setLanguage] = useState(savedLanguage ?? "br");
+  const [languages, setLanguages] = useState(); //estado duplicado
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetcLanguages = async () => {
-            try {
-                const getTexts = await getApiData('webtext')
-                setLanguages(getTexts)
-            } catch (e) {
-                console.error(e)
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetcLanguages()
-    }, [])
+  useEffect(() => {
+    const fetcLanguages = async () => {
+      try {
+        const getTexts = await getApiData("webtext");
+        setLanguages(getTexts);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetcLanguages();
+  }, []);
 
-    useEffect(() => {
-        localStorage.setItem('lang', language)
-    }, [language])
+  useEffect(() => {
+    localStorage.setItem("lang", language);
+  }, [language]);
 
-    return (
-        <AppContext.Provider value={{language, languages, setLanguage, loading}}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={{ language, languages, setLanguage, loading }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
